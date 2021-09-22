83b4447c15e9ada9b494b2d78273396e3656182b
Andrey Vinokurov <kreketjot@gmail.com>
2021-09-22 13:26:43 +0300

    TypeController added

--- server/controllers/TypeController.js
+++ server/controllers/TypeController.js
@@ -1,10 +1,16 @@
+const { Type } = require('../models/models');
+const ApiError = require('../errors/ApiError');
+
 class TypeController {
   static async create(req, res) {
-
+    const { name } = req.body;
+    const type = await Type.create({ name });
+    return res.json(type);
   }
 
   static async getAll(req, res) {
-
+    const types = await Type.findAll();
+    return res.json(types);
   }
 }
 
